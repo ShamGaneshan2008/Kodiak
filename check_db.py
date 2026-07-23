7b8360a7eb70473a74a83439a36cff45608e1f10
Sham Ganeshan SB <shamganeshansb@gmail.com>
2026-07-23 15:46:20 +0530

    check_db.py has been upodated"

--- check_db.py
+++ check_db.py
@@ -3,11 +3,13 @@ from sqlalchemy import create_engine, text
 engine = create_engine("postgresql://kodiak:kodiak@localhost:5432/kodiak")
 
 with engine.connect() as conn:
-    result = conn.execute(text("""
+    result = conn.execute(
+        text("""
         SELECT table_name 
         FROM information_schema.tables 
         WHERE table_schema = 'public'
-    """))
+    """)
+    )
 
     for row in result.fetchall():
         print(row[0])
