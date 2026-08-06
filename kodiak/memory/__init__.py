167c6e771234f4a02965db399f17952d2bfaf4a1
Sham Ganeshan SB <shamganeshansb@gmail.com>
2026-08-06 18:24:31 +0530

    The file __init__.py has been updated

---
+++ kodiak/memory/__init__.py
@@ -0,0 +1,102 @@
+# kodiak/memory/__init__.py
+"""Kodiak Memory System package.
+
+Provides Working Memory, Short-Term Memory, Long-Term Memory (Episodic,
+Semantic, Procedural), Retrieval, Ranking, Context Building, Persistence,
+and Consolidation.
+"""
+
+from .consolidation import ConsolidationResult, ConsolidationStatus, MemoryConsolidator
+from .context import MemoryContextBuilder
+from .episodic import Episode, EpisodeRepository, EpisodeSearchResult, EpisodicMemory
+from .errors import (
+    EpisodeNotFoundError,
+    FactNotFoundError,
+    MemoryNotFoundError,
+    MemoryPersistenceError,
+    MemoryServiceError,
+    ProcedureNotFoundError,
+    ShortTermMemoryError,
+    WorkingMemoryNotFoundError,
+)
+from .long_term import LongTermMemory
+from .models import Memory, MemoryType, SearchResult
+from .persistence import (
+    InMemoryEpisodeRepository,
+    InMemoryProcedureRepository,
+    InMemorySemanticRepository,
+    InMemoryShortTermMemoryRepository,
+    InMemoryWorkingMemoryRepository,
+    JSONFileMemoryPersistence,
+)
+from .procedural import (
+    ProceduralMemory,
+    Procedure,
+    ProcedureRepository,
+    ProcedureSearchResult,
+    ProcedureStep,
+)
+from .ranking import MemoryRanker
+from .retrieval import MemoryRetriever
+from .semantic import (
+    FactNotFoundError,
+    SemanticEntity,
+    SemanticMemory,
+    SemanticRepository,
+    SemanticSearchResult,
+)
+from .short_term import ShortTermMemory, ShortTermMemoryItem, ShortTermMemoryRepository
+from .service import MemoryService
+from .working import WorkingMemory, WorkingMemoryItem, WorkingMemoryRepository, WorkingMemoryStatus
+
+__all__ = [
+    # Models
+    "Memory",
+    "MemoryType",
+    "SearchResult",
+    "WorkingMemoryItem",
+    "WorkingMemoryStatus",
+    "ShortTermMemoryItem",
+    "Episode",
+    "EpisodeSearchResult",
+    "SemanticEntity",
+    "SemanticSearchResult",
+    "Procedure",
+    "ProcedureStep",
+    "ProcedureSearchResult",
+    "ConsolidationResult",
+    "ConsolidationStatus",
+    # Subsystems & Managers
+    "WorkingMemory",
+    "ShortTermMemory",
+    "EpisodicMemory",
+    "SemanticMemory",
+    "ProceduralMemory",
+    "LongTermMemory",
+    "MemoryRetriever",
+    "MemoryRanker",
+    "MemoryContextBuilder",
+    "MemoryConsolidator",
+    "MemoryService",
+    # Repositories & Persistence
+    "InMemoryWorkingMemoryRepository",
+    "InMemoryShortTermMemoryRepository",
+    "InMemoryEpisodeRepository",
+    "InMemorySemanticRepository",
+    "InMemoryProcedureRepository",
+    "JSONFileMemoryPersistence",
+    "WorkingMemoryRepository",
+    "ShortTermMemoryRepository",
+    "EpisodeRepository",
+    "SemanticRepository",
+    "ProcedureRepository",
+    # Exceptions
+    "MemoryServiceError",
+    "MemoryNotFoundError",
+    "WorkingMemoryNotFoundError",
+    "EpisodeNotFoundError",
+    "FactNotFoundError",
+    "ProcedureNotFoundError",
+    "ShortTermMemoryError",
+    "MemoryPersistenceError",
+]
